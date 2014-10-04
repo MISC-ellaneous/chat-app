d25018dcbb7611f7354d3d78fb4ac912c001c874
Talon Poole <talon.poole@gmail.com>
2014-10-03 20:48:32 -0600

    ember cli works

--- app/index.js
+++ app/index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var path = require('path');
+
+app.use(express.static('public'));
+app.use(express.static('public/dist'));
 
 io.on('connection', function(){
 
@@ -9,7 +13,10 @@ io.on('connection', function(){
 
 
 app.get('*', function (req, res) {
-  res.sendfile('./public/dist/index.html');
+  res.set('Content-Type', 'text/html');
+  res.sendFile(path.resolve('./public/dist/index.html'));
 });
 
-server.listen(3000);
+server.listen(3000, function () {
+  console.log('server on port 3000');
+});
